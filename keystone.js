7d9cc9408299016b370ecea346669ae66799fa5f
talpidaex <bayrakogzhn@gmail.com>
2019-09-17 22:53:08 +0300

    mobile device design

--- keystone.js
+++ keystone.js
@@ -11,7 +11,7 @@ keystone.init({
   'name': 'Testing', //DB adı
   'auto update': true,
   'auth': true,
-  'port': 80,
+  'port': 3000,
   'user model': 'User', //kullanılacak collection adı
 
 });
